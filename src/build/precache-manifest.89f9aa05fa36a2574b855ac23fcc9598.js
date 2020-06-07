418123a7871a0836e278cce715c13068dd7a031c
Marcela Pozo <someone@nowhere.com>
2020-06-06 21:13:53 -0600

    first-commit

---
+++ src/build/precache-manifest.89f9aa05fa36a2574b855ac23fcc9598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0e42454eede5e09fcb3803f4dcd1c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f66d22efd0182e03d4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "419da92b8be217ddae69",
+    "url": "/static/css/main.cdc9d560.chunk.css"
+  },
+  {
+    "revision": "b5f66d22efd0182e03d4",
+    "url": "/static/js/2.ced79227.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ced79227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419da92b8be217ddae69",
+    "url": "/static/js/main.87df160d.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
